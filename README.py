2df8dcfe9711d51fd251db4e875b1814178e3127
Deepak Kumar S <142745213+DeepakS024@users.noreply.github.com>
2024-10-27 16:15:30 +0530

    Update and rename README.md to README.py

---
+++ README.py
@@ -0,0 +1,7 @@
+print(-  Hi, I’m @DeepakS024)
+print(I’m interested in Cyber Security and Cloud Computing)
+print(-  I’m currently learning Information Science and Engineering)
+print(-  I’m looking to collaborate on Cyber Security Analyst role)
+print(-  How to reach me : Google me)
+
+
